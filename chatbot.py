dbcf34773cb9e152b5771927bb586ab8d0a89e4f
Iaaayush <ayushmishra9322@gmail.com>
2026-03-31 16:36:51 +0530

    added web search capability

--- chatbot.py
+++ chatbot.py
@@ -1,16 +1,31 @@
 from groq import Groq
+from tavily import TavilyClient
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+groq_client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+tavily_client= TavilyClient(api_key=os.getenv("TAVILY_API_KEy"))
+
+def search_web(query):
+    results = tavily_client.search(query, max_results=3)
+    
+    search_data = ""
+    for result in results["results"]:
+        search_data += f"Source: {result['url']}\n"
+        search_data += f"Content: {result['content']}\n\n"
+    return search_data
+
+
 
 messages = [
-    {"role":"system","content": "You are Someone who knows everything. keep answering everything short and clear"}
+    {"role":"system","content": """"You are a helpful assistant with web search ability.
+     When answering, use the search results provided to give accurate up to date answers.
+     Always mention where you got the information from."""}
 ]
 
-print("======Chatbot======")
+print("======Chatbot with Web Search======")
 print("Type 'quit' to exit\n")
 
 while True:
@@ -22,9 +37,21 @@ while True:
         print("Bye!")
         break
 
-    messages.append({"role": "user", "content": user_input})
 
-    response = client.chat.completions.create(
+        print("🔍 Searching web...")
+    search_results = search_web(user_input)
+
+    # Add search results + user question together
+    combined = f"""User question: {user_input}
+
+Here are relevant search results:
+{search_results}
+
+Answer the question using these search results."""
+
+    messages.append({"role": "user", "content": combined})
+
+    response = groq_client.chat.completions.create(
     model="llama-3.3-70b-versatile",
     messages=messages
     )
@@ -33,4 +60,4 @@ while True:
 
     messages.append({"role": "assistant", "content": reply})
 
-    print(f"\nAi: {reply}\n")
+    print(f"\nAI: {reply}\n")
