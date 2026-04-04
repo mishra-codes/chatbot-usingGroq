4daa5a042e2dde38ea338188a5c4b5ecbdd49c2e
Iaaayush <ayushmishra9322@gmail.com>
2026-04-04 12:57:42 +0530

    fixed tool use model

--- chatbot.py
+++ chatbot.py
@@ -64,7 +64,7 @@ while True:
 
     #First AI call - whether it should use web search?
     response = groq_client.chat.completions.create(
-        model="llama-3.3-70b-versatile",
+        model="moonshotai/kimi-k2-instruct",
         messages=messages,
         tools=tools,
         tool_choice = "auto"
@@ -73,37 +73,43 @@ while True:
     response_message = response.choices[0].message
 
     #Checks if AI want to use tool
-    if response_message.tool_calls:
-        print(" AI decided to Search....")
+    try:
+        if response_message.tool_calls:
+            print(" AI decided to Search....")
 
-        #getting the tool call details
-        tool_call = response_message.tool_calls[0]
-        tool_name = tool_call.function.name
-        tool_args = json.loads(tool_call.function.arguments)
+           #getting the tool call details
+            tool_call = response_message.tool_calls[0]
+            tool_name = tool_call.function.name
+            tool_args = json.loads(tool_call.function.arguments)
 
-        #Execute the web search
-        search_results = search_web(tool_args["query"])
+           #Execute the web search
+            search_results = search_web(tool_args["query"])
 
-        #Add AI decision + searcg results to history
-        messages.append(response_message)
-        messages.append({
-            "role": "tool",
-            "tool_call_id": tool_call.id,
-            "content": search_results
-        })
+           #Add AI decision + searcg results to history
+            messages.append(response_message)
+            messages.append({
+                "role": "tool",
+                "tool_call_id": tool_call.id,
+                "name": "search_web",
+                "content": search_results
+            })
 
-        # Second AI call - now Ai has search results, generates final answer
-        final_response = groq_client.chat.completions.create(
-            model="llama-3.3-70b-versatile",
-            messages=messages
-        )
+            # Second AI call - now Ai has search results, generates final answer
+            final_response = groq_client.chat.completions.create(
+                model="moonshotai/kimi-k2-instruct",
+                messages=messages
+            )
 
-        reply = final_response.choices[0].message.content
+            reply = final_response.choices[0].message.content
 
-    else:
-        #AI answered from memory
-        print(" AI answered from memory")
-        reply = response_message.content   
+        else:
+            #AI answered from memory
+            print(" AI answered from memory")
+            reply = response_message.content   
+
+    except Exception as e:
+        print(f"Error: {e}")
+        reply = "Something went Wrong , please try again."
 
     messages.append({"role": "assistant", "content": reply})
     print(f"\nAI: {reply}\n")
